eb49ba951c0cc863cb1cd5b726108a06311a1120
xbukovsk <xbukovsk@gmail.com>
2022-03-14 18:04:51 +0100

    #00000 With small updates before final implementation of promo action

--- src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
+++ src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
@@ -9,13 +9,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
-public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
-    public static final double basicCallTax = 1;
-    public static final double callTaxOutOfInterval = 0.5;
-    public static final double callTaxMoreThanFiveMinutes = 0.2;
-
-    // Vstup metody bude splnovat nize uvedena kriteria
-    /*
+// Vstup metody bude splnovat nize uvedena kriteria
+/*
         Vstupem metody je textový řetězec obsahující výpis volání. Výpis volání je ve formátu CSV s následujícími poli:
         • Telefonní číslo v normalizovaném tvaru obsahující pouze čísla (např. 420774567453)
         • Začátek hovoru ve tvaru dd-MM-yyyy HH:mm:ss
@@ -24,11 +19,14 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
         Příklad obsahu souboru:
         420774577453,13-01-2020 18:10:15,13-01-2020 18:12:57 --> 3 minuty po 0.5 --> 1.5
         420776562353,18-01-2020 08:59:20,18-01-2020 09:01:00 --> 2 minuta po 1 --> 2
-        420776562353,19-01-2020 08:59:20,20-01-2020 09:10:00 --> SILENA SUMA, jsou to rozdilne dny!
+        420776562353,19-01-2020 08:59:20,20-01-2020 09:10:00 --> SILENA SUMA, jsou to rozdilne dny! (5 minut po 1 + 1446 minut po 0.2 --> 294.2)
         420776562353,19-01-2020 08:59:20,19-01-2020 09:10:00 --> 5 minut po 1 + 6 minut po 0.2 --> 6.2
         420776562353,27-01-2020 04:59:20,27-01-2020 05:10:00 --> 5 minut po 0.5 + 6 minut po 0.2 --> 2.5 + 1.2 = 3.7
         420776562353,27-01-2020 07:57:25,27-01-2020 08:03:42 --> 3 minuty po 0.5 + 2 minuty po 1 + 2 minuty po 0.2 --> 3.9
         420776562353,27-01-2020 15:57:25,27-01-2020 16:01:42 --> 3 minuty po 1 + 2 minuty po 0.5 --> 4.0
+
+        BEGIN 07:57:25 with END 08:03:42 --> 7 minutes
+        OR BEGIN 15:57:25 with END 16:03:42 --> 7 minutes
 
         Výstupem metody je částka k uhrazení spočtena dle vstupního výpisu dle následujících pravidel:
     • Minutová sazba v intervalu <8:00:00,16:00:00) je zpoplatněna 1 Kč za každou započatou minutu.
@@ -40,23 +38,24 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
 
     • V rámci promo akce operátora dále platí, že hovory na nejčastěji volané číslo v rámci výpisu nebudou zpoplatněny.
     V případě, že by výpis obsahoval dvě nebo více takových čísel, zpoplatněny nebudou hovory na číslo s aritmeticky nejvyšší hodnotou.
-     */
+ */
 
-    // TODO tohle jeste upravit, jednou pouzivat minutes a podruhe sahat na minutesEnd () --> nelze to udelat jednoduseji? --> prvne specificke pripady
+public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
+    public static final double BASIC_CALL_TAX = 1;
+    public static final double OUT_OF_BASIC_INTERVAL_CALL_TAX = 0.5;
+    public static final double MORE_THAN_FIVE_MINUTES_CALL_TAX = 0.2;
+
+    // 4 varianty 1) zapocne a skonci mimo hlavni tarif ++ 2) zapocne a skonci v hlavnim tarifu ++ 3) zapocne mimo tarif a skonci v nem ++ 4) zapocne v tarifu a skonci mimo nej
     private double getBasicSumOfBill(LocalDateTime callBegin, LocalDateTime callEnd, long minutes) {
         if (((callBegin.getHour() == 7) && (callEnd.getHour() == 8)) || ((callBegin.getHour() == 15) && (callEnd.getHour() == 16))) { // specificke pripady, okrajove hodnoty
-            double tax = callBegin.getHour() == 7 ? 0.5 : 1;
-            long minutesBegin = 60 - callBegin.getMinute(); //BEGIN 7:57:25 NEBO 15:57:25 --- END --- 8:03:42 --> 7 minut NEBO 16:03:42 --> 7 minut
-            double secondTax = tax == 0.5 ? 1 : 0.5;
+            double tax = callBegin.getHour() == 7 ? OUT_OF_BASIC_INTERVAL_CALL_TAX : BASIC_CALL_TAX;
+            long minutesBegin = 60 - callBegin.getMinute();
+            double secondTax = (tax == OUT_OF_BASIC_INTERVAL_CALL_TAX) ? BASIC_CALL_TAX : OUT_OF_BASIC_INTERVAL_CALL_TAX;
             long minutesEnd = minutes - minutesBegin;
 
-            System.out.println("FIRST IF " + minutes);
-
             return (minutesBegin * tax) + (minutesEnd * secondTax);
-
         } else {
-            double tax = ((callEnd.getHour() < 8) || (callBegin.getHour() > 16)) ? 0.5 : 1;
-            System.out.println("SECOND IF with tax " + minutes * tax);
+            double tax = ((callEnd.getHour() < 8) || (callBegin.getHour() > 16)) ? OUT_OF_BASIC_INTERVAL_CALL_TAX : BASIC_CALL_TAX;
             return minutes * tax;
         }
     }
@@ -70,25 +69,21 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
         LocalDateTime callBegin = LocalDateTime.parse(calls[1], formatter);
         LocalDateTime callEnd = LocalDateTime.parse(calls[2], formatter);
 
-        long minutes = ChronoUnit.MINUTES.between(callBegin, callEnd);
         long seconds = ChronoUnit.SECONDS.between(callBegin, callEnd);
 
-        if (seconds > 300) { // resi bod 2, tedy hovory s delsim casovym rozpetim nez 5 minut a se slevou
-            System.out.println("first");
-
+        // resi bod 2, tedy hovory s delsim casovym rozpetim nez 5 minut a se slevou
+        if (seconds > 300) {
             long cheaperSeconds = seconds - 300;
             LocalDateTime updatedCallEnd = callBegin.plus(Duration.of(301, ChronoUnit.SECONDS));
 
-            int mins = (int) (cheaperSeconds / 60.0);
+            int minutes = (int) (cheaperSeconds / 60.0);
             sum = getBasicSumOfBill(callBegin, updatedCallEnd, 5);
-            sum += (cheaperSeconds % 60 > 0 ? mins+1 : mins) * 0.2;
-        } else { // 4 varianty 1) zapocne a skonci mimo hlavni tarif ++ 2) zapocne a skonci v hlavnim tarifu ++ 3) zapocne mimo tarif a skonci v nem ++ 4) zapocne v tarifu a skonci mimo nej
-            System.out.println("second");
+            sum += (cheaperSeconds % 60 > 0 ? minutes+1 : minutes) * MORE_THAN_FIVE_MINUTES_CALL_TAX;
+        } else {
+            int minutes = (int) (seconds / 60.0);
+            minutes = (seconds % 60 > 0 ? minutes+1 : minutes);
 
-            int mins = (int) (seconds / 60.0);
-            mins = (seconds % 60 > 0 ? mins+1 : mins);
-
-            sum = getBasicSumOfBill(callBegin, callEnd, mins);
+            sum = getBasicSumOfBill(callBegin, callEnd, minutes);
         }
 
         System.out.println(sum);
@@ -104,9 +99,11 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator {
         try {
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
             while ((line = br.readLine()) != null) {
-                // TODO ma to vystup metody
                 telephoneBillCalculator.calculate(line);
             }
+
+            // TODO ma to vystup metody --> zde tedy provest vypis jednotlivych cisel s jejich sazbou hovoru
+
         } catch (IOException e) {
             e.printStackTrace();
         }
